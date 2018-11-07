8978d3b974ee643249a7445387267aa9991ac363
Delilah <delstuff@gmail.com>
2018-11-07 15:19:41 +0100

    ActiveOnCamera updated

--- scripts/ActiveOnCamera.js
+++ scripts/ActiveOnCamera.js
@@ -1,5 +1,6 @@
 // Use in "Initialized' event in order to display or hide elements when the front/back camera is selected
 // Add as script component to your object and then check the options you need
+// Note: does not work properly on iOS. Please use VisibleOnCamera instead: https://github.com/delilah/lens-studio-ar/blob/master/scripts/VisibleOnCamera.js
 
 //@input SceneObject target
 
@@ -9,12 +10,12 @@
 
 script.createEvent('CameraBackEvent').bind(function (eventData)
 
-{
-    script.target.enabled = script.activeOnBack;
-});
+    {
+        script.target.enabled = script.activeOnBack;
+    });
 
 script.createEvent('CameraFrontEvent').bind(function (eventData)
 
-{
-    script.target.enabled = script.activeOnFront;
-});
+    {
+        script.target.enabled = script.activeOnFront;
+    });
